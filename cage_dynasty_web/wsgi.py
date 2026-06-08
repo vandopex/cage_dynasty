814673fefcf0afa23d421f5d5407027256fb7291
vandopex <Donovanfleming@gmail.com>
2026-06-08 16:05:48 -0700

    Fix: force-clear CLI module cache in wsgi.py before importing app

--- cage_dynasty_web/wsgi.py
+++ cage_dynasty_web/wsgi.py
@@ -1,13 +1,18 @@
 import sys
+import os
 
 game_root    = '/home/vandopegaming/cage_dynasty'
 project_home = '/home/vandopegaming/cage_dynasty/cage_dynasty_web'
 
-# project_home MUST come before game_root so web app modules
-# shadow CLI modules with the same name (fight_engine, game_state etc.)
+# project_home MUST come before game_root
 if game_root not in sys.path:
     sys.path.append(game_root)
 if project_home not in sys.path:
     sys.path.insert(0, project_home)
+
+# Force web versions of modules that exist in both web and CLI
+for mod in ['fight_engine', 'fight_integration', 'game_state']:
+    if mod in sys.modules:
+        del sys.modules[mod]
 
 from app import app as application
