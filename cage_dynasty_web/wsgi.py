1eed1a09fba2e254c4aa21a76351f97bec4eee9a
vandopex <Donovanfleming@gmail.com>
2026-06-08 15:58:04 -0700

    Fix: add game_root to sys.path in wsgi.py to load systems/ and narrative/

--- cage_dynasty_web/wsgi.py
+++ cage_dynasty_web/wsgi.py
@@ -1,10 +1,10 @@
-"""WSGI entry point for PythonAnywhere deployment.
-PA's web app config should point to this file.
-"""
 import sys
 
 project_home = '/home/vandopegaming/cage_dynasty/cage_dynasty_web'
-if project_home not in sys.path:
-    sys.path.insert(0, project_home)
+game_root = '/home/vandopegaming/cage_dynasty'
+
+for path in [project_home, game_root]:
+    if path not in sys.path:
+        sys.path.insert(0, path)
 
 from app import app as application
